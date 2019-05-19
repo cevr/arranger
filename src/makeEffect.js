b9317aada87026c2e46dab8b9a76c133d29bb3db
cevr <seeve.c@gmail.com>
2019-05-18 23:07:52 -0400

    fix: fix issue that would cause makeEffect to throw error if deps undefined

--- src/makeEffect.js
+++ src/makeEffect.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 import pick from './utils/pick'
 
 const makeEffect = (effect, deps) => (props = {}) => {
-    const effectDeps = pick(deps, props)
+    const effectDeps = deps ? pick(deps, props) : undefined
     useEffect(() => effect(props), effectDeps)
 
     return props
